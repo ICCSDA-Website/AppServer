0c329a138479aef521d11f5253559ff6792b8f49
Marcus Rodney <marcus.rodney@hotmail.co.uk>
2017-03-05 20:36:02 +0000

    add scrolling navbar code to head

--- src/main/java/Server/Inter.java
+++ src/main/java/Server/Inter.java
@@ -66,6 +66,33 @@ public class Inter extends HandlerInterceptorAdapter {
 				+ " " + "crossorigin=\"anonymous\"></script>");
 		head.append("\n");
 		head.append("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>");
+		head.append("\n");
+		head.append("<script>var headerHeight = 168;"
+							+"var isShown = false;"
+							+"$(window).bind('scroll', function() {"
+								+"var top = $(window).scrollTop();"
+								+"console.log(top);"
+									+"if (!isShown && top > headerHeight) {"
+										+"$('body').addClass('stop-scrolling')"
+										+"$('#myNav').hide().slideDown(400, function() {"
+											+"$('#myNav').removeClass('navbar-static-top').addClass('navbar-fixed-top');"
+											+"$('body').removeClass('stop-scrolling');"
+											+"isShown = true;"
+											+"console.log('added fix-top');"
+										+ "});"
+										+"$('body').addClass('stop-scrolling')"
+									+"}"
+									+ "else if ($(window).scrollTop() == 0) {"
+										+"setStaticTop();"
+									+"}"
+							+"});"
+							+""
+							+ "function setStaticTop() {"
+								+"$('#myNav').removeClass('navbar-fixed-top').addClass('navbar-static-top');"
+								+"isShown = false;"
+								+"console.log('remove fixed-top');"
+							+"}"
+							+ "</script>");
 		
 		model.addObject("head", head.toString());
 		         
